d79da9ce4bcf0aa3626cc4910491a9296257daba
Seong Gi Dong <dirrksdl@naver.com>
2019-12-18 22:58:12 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
                   <div className="page-1-content">
                     <p className="page-1-p">안녕하세요!</p>
                     <h1>풀스택 개발자 <strong>성기동</strong>의 포트폴리오 웹사이트입니다.</h1>
-                    <p><a className="btn btn-dark" href="https://github.com/sgd122" role="button" target="_blank">
+                    <p><a className="btn btn-dark" href="https://github.com/sgd122/portfolio-react" role="button" target="_blank">
                       <span className="fa fa-github" aria-hidden="true"></span>
                       &nbsp; Github</a></p>
                   </div>
